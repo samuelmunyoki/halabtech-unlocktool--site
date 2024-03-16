2955ab71d1f8ee76e8021cec15e691642c56e265
Samuel Munyoki Muthembwa <samuelmuthembwa@yahoo.com>
2024-03-16 22:59:21 +0300

    Update SalesComponent.tsx

--- components/sales/SalesComponent.tsx
+++ components/sales/SalesComponent.tsx
@@ -6,7 +6,7 @@ const SalesComponent = () => {
   return (
     <>
       <div className="bg-blue-700 py-3 w-[60vw] mx-auto mt-10 flex flex-row items-center justify-center mb-4">
-        <p className="text-xl text-white">Our Packages</p>
+        <p className="text-xl text-white">Halabtech Packages</p>
       </div>
       <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 md:grid-flow-row md:gap-x-8 md:gap-y-10 mx-auto mt-6">
         {dummyPackages.map((packageItem, index) => (
